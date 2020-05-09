cd7fdc050d2da8fe59a56d7bcb039a283d681021
tutu524606 <524682015@qq.com>
2020-05-09 12:46:02 +0800

    测试2

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
  # @Author: Dai Junchao
  # @Date: 2020-05-09 12:40:04
  # @LastEditors: Dai Junchao
- # @LastEditTime: 2020-05-09 12:41:39
+ # @LastEditTime: 2020-05-09 12:45:59
  ###
-npm run dev
-# git add .
-# git commit -m '测试2'
-# git pull
-# git push
+
+git add .
+git commit -m '测试2'
+git pull
+git push
